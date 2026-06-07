978fd7f7b23fab9296ff693d1806722fdf9822cc
Matthew Loria <loria.matt@gmail.com>
2026-06-06 20:43:36 -0400

    Deploy to gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from '@astrojs/react';
 // Update `site` to your GitHub Pages URL (e.g. 'https://yourusername.github.io')
 // Update `base` to '/' once you have a custom domain pointed at GitHub Pages
 export default defineConfig({
-  site: 'https://github.io',
+  site: 'https://mtloria.github.io',
   base: '/summitt-wellness',
   integrations: [react()],
   output: 'static',
