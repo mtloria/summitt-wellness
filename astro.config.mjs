f7a02d75e421e947dabdf35ccca3a99e6eb60058
Matthew Loria <loria.matt@gmail.com>
2026-07-02 16:41:39 -0400

    CNAME and update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
-// Update `site` to your GitHub Pages URL (e.g. 'https://yourusername.github.io')
-// Update `base` to '/' once you have a custom domain pointed at GitHub Pages
 export default defineConfig({
-  site: 'https://mtloria.github.io',
-  base: '/summitt-wellness',
+  site: 'https://summittwellness.com',
+  base: '/',
   integrations: [react()],
   output: 'static',
 });
